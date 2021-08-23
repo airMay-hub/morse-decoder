24b726841e23088a186e66a94d5fb94fd910bd66
airMay-hub <kiiikaaa.93@mail.ru>
2021-08-23 23:41:13 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = [];
+  for(let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.substr(i, 10));
+  }
+
+  let arr2 = [];
+  arr.forEach(elem => {
+    let str = [];
+    for(let i = 0; i < elem.length; i += 2) {
+      str.push(elem.substr(i, 2).split());
+    }
+    arr2.push(str.map(item => item.join()));
+  });
+
+  let obj = {
+    "00": " ",
+    "10": ".",
+    "11": "-",
+    "**": "**"
+  };
+  
+  arr2.forEach(element => {
+    for(let i = 0; i < element.length; i++) {
+      for (let key of Object.keys(obj)) {
+        if(key === element[i]) {
+          element[i] = obj[key];
+        }
+      }
+    }
+  });
+  arr2 = arr2.map(item => item.join('').trim());
+  let string = [];
+  arr2.forEach(element => {
+    if(element === '**********') {
+      element = ' ';
+      string.push(element);
+    }
+    for (const key of Object.keys(MORSE_TABLE)) {
+      if(key === element) {
+        element = MORSE_TABLE[key];
+        string.push(element);
+      }
+    }
+  });
+  return string.join('');
 }
 
 module.exports = {
